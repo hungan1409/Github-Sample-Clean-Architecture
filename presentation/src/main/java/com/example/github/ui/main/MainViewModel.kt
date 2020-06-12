82d95d8400c1f933a637c5a5e790d16734dcb842
huuphuoc1396 <huuphuoc1396@gmail.com>
2020-06-12 22:47:23 +0700

    Create multi-type adapter for RecyclerView

--- presentation/src/main/java/com/example/github/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/example/github/ui/main/MainViewModel.kt
@@ -2,16 +2,33 @@ package com.example.github.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import com.example.github.base.BaseViewModel
+import com.example.github.base.ModelItem
+import com.example.github.model.PageHeaderItem
 import com.example.github.model.RepoItem
-import com.example.github.model.RepoItemMapper
+import com.example.github.model.UserItem
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(
-    private val repoItemMapper: RepoItemMapper
-) : BaseViewModel() {
+class MainViewModel @Inject constructor() : BaseViewModel() {
 
-    val data = MutableLiveData<List<RepoItem>>()
-    val query = MutableLiveData<String>()
+    val fakeData = MutableLiveData<List<ModelItem>>()
     val loading = MutableLiveData<Boolean>().apply { postValue(false) }
 
+    fun getFakeData() {
+        Thread {
+            Thread.sleep(2000)
+            fakeData.postValue(
+                listOf(
+                    UserItem("", "", "", 1, "", 1),
+                    PageHeaderItem(1),
+                    RepoItem("", 1, 1, "", "", 1, null),
+                    RepoItem("", 1, 2, "", "", 1, null),
+                    RepoItem("", 1, 3, "", "", 1, null),
+                    PageHeaderItem(2),
+                    RepoItem("", 1, 4, "", "", 1, null),
+                    RepoItem("", 1, 5, "", "", 1, null),
+                    RepoItem("", 1, 6, "", "", 1, null)
+                )
+            )
+        }.start()
+    }
 }
